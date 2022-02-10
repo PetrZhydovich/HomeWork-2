9e72aba1578c24575df7fac6744d1696901b9632
Petr <povar_cr@mail.ru>
2022-02-10 13:59:34 +0300

    add Example_001

---
+++ Example_001/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Показать вторую цифру трёхзначного числа.
+int randomNumber = new Random().Next(100,999);
+Console.WriteLine($"Случайное трёхзначное число:  {randomNumber}");
+char[] array = randomNumber.ToString().ToCharArray();
+var char1 = array[1];
+Console.WriteLine($"Втоая цифра трёхзначного числа: {char1}");
+
+
+
+
+
