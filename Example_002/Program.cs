ee164b8ccc8533c8dbe3187b552d22568d065ca2
Petr <povar_cr@mail.ru>
2022-02-10 14:11:46 +0300

    add Example_002

---
+++ Example_002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+int randomNumber = new Random().Next(100,999);
+Console.WriteLine($"Случайное трёхзначное число:  {randomNumber}");
+char[] array = randomNumber.ToString().ToCharArray();
+var char1 = array[0];
+var char2 = array[1];
+var char3 = array[2];
+Console.WriteLine($"Удалили вторую цифру трехзначного числа: {char1} {char3}");
