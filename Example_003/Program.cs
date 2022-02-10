23a5357bedf68369631435d1dad04b694d269cb7
Petr <povar_cr@mail.ru>
2022-02-10 15:06:04 +0300

    add Example_003

---
+++ Example_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+int GetRandom()
+{
+int randomNumber = new Random().Next(1,150);
+return randomNumber;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+int result = randomazer /10;
+if(result >= 10)
+{
+    Console.WriteLine(randomazer % 10);
+}
+else
+{
+    Console.WriteLine("Нет 3-ей цифры!");
+}
